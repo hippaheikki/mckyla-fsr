e510658a038e9f486bb19f7957060caacb74a782
Sereni <sereni.nm@gmail.com>
2019-06-13 19:37:02 +0100

    Add status message to indicate that the web UI is running.

--- web-ui/server.py
+++ web-ui/server.py
@@ -4,11 +4,18 @@ import BaseHTTPServer
 import CGIHTTPServer
 import cgitb
 cgitb.enable()  ## This line enables CGI errorreporting
- 
+
+name = ""
+port = 80
+
 server = BaseHTTPServer.HTTPServer
 handler = CGIHTTPServer.CGIHTTPRequestHandler
-server_address = ("", 80)
+server_address = (name, port)
 handler.cgi_directories = ["/pad1", "/pad2", "/cgi-bin"]
+
+if name == "":
+  name = "localhost"
+print "Staring server. Visit http://%s:%s" % (name, port)
  
 httpd = server(server_address, handler)
 httpd.serve_forever()
